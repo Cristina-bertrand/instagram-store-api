13e48a51a25bb8236338832bd0790e4b2e352731
Cristina Bertrand <cristina@11870.com>
2018-03-24 17:04:22 +0100

    feat:users create in route and controller with api-error model

--- app.js
+++ app.js
@@ -5,16 +5,14 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 require('./configs/db.config');
 
-const users = require('./routes/users');
+const usersRoutes = require('./routes/users.route');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,24 +22,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/users', users);
+app.use('/users', usersRoutes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use((req, res, next)  => {
+  const error = new Error('Not Found');
+
+  error.status = 404;
+  next(error);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ message: error.message || '' });
 });
 
 module.exports = app;
