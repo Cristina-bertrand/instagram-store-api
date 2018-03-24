55b80280d22ba95ab8774842c847909f387464e7
Cristina Bertrand <cristina@11870.com>
2018-03-24 16:19:35 +0100

    feat:db.config

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+require('./configs/db.config');
 
 const users = require('./routes/users');
 
